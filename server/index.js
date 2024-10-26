65583f50c90113824768ec60db6c15525352aa61
Udit Narayan Pagad <112816480+UditNarayanPagad@users.noreply.github.com>
2024-10-26 09:49:05 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,18 +2,31 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors"
 import dotenv from "dotenv";
+import path from "path"
 import mongoose from "mongoose";
 import authRoutes from "./routes/AuthRoute.js";
 import contactRoutes from "./routes/ContactRoute.js";
 import setupSocket from "./socket.js";
 import messagesRoutes from "./routes/MessagesRoute.js";
 import channelRoutes from "./routes/ChannelRoute.js";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3001;
 const databaseUrl = process.env.DATABASE_URL
+
+// Resolve the __dirname equivalent in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve static files
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
 
 app.use(
     cors({
