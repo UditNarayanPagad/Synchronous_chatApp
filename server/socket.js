f4efdaa4c3b893bbed3725cafb9502e77d68151f
Udit Narayan Pagad <112816480+UditNarayanPagad@users.noreply.github.com>
2024-10-26 11:59:15 +0530

    Update socket.js

--- server/socket.js
+++ server/socket.js
@@ -5,7 +5,9 @@ import Channel from "./models/ChannelModel.js"
 const setupSocket = (server) => {
     const io = new SocketIOServer(server,{
         cors:{
-            origin: process.env.ORIGIN,
+            origin:  [
+        "https://synchronous-chatapp.onrender.com","http://localhost:5173"
+    ],
             methods: ["GET","POST"],
             credentials: true,
         },
