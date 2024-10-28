f90b23c84fa012c19a7215693b1a9de74ce58924
Udit Narayan Pagad <112816480+UditNarayanPagad@users.noreply.github.com>
2024-10-28 23:59:19 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -58,11 +58,11 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Serve static files
-// app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-// });
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
 
 
 app.use('/uploads/profiles', express.static('uploads/profiles'));
