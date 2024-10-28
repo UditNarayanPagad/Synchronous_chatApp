c3e43093691ab8d08f37948b0c736fa345ec27be
Udit Narayan Pagad <112816480+UditNarayanPagad@users.noreply.github.com>
2024-10-28 18:00:10 +0530

    Update AuthMiddleware.js

--- server/middlewares/AuthMiddleware.js
+++ server/middlewares/AuthMiddleware.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
     const token = req.cookies.jwt;
-    console.log(token);
+    console.log("Token:", token);
     if (!token) {
         return res.status(401).send("You are not authenticated");
     }
