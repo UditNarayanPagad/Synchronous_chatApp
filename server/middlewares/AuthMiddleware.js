c9b05dcf240dd5478833e84432265b0c0cd1efa7
Udit Narayan Pagad <112816480+UditNarayanPagad@users.noreply.github.com>
2024-10-29 09:23:14 +0530

    Update AuthMiddleware.js  consoled

--- server/middlewares/AuthMiddleware.js
+++ server/middlewares/AuthMiddleware.js
@@ -13,6 +13,7 @@ export const verifyToken = (req, res, next) => {
         }
         req.userId = payload.userId;
         console.log("Payload:", payload);
+        console.log("req.userId",req.userId);
         next();
     });
 };
